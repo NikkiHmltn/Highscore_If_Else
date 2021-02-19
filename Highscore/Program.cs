df4da1cf08767d0acbaff861f0931c6ee0b31689
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-18 22:55:52 -0800

    set up method to comapre scores to current highscore and replace if higher

--- Highscore/Program.cs
+++ Highscore/Program.cs
@@ -6,7 +6,45 @@ namespace Highscore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //create an app with a score, highscore, and a highscorePlayer
+            //create a method with two params, one for score and the other for playerName
+            //whenmethod is called, check if the score of the player is higher than thehighscore
+            int score;
+            string playerName;
+            bool scoreCheck;
+            Console.WriteLine("What is your name?");
+            playerName = Console.ReadLine();
+            Console.WriteLine("What is your score?");
+            string scoreInput = Console.ReadLine();
+            scoreCheck = int.TryParse(scoreInput, out score);
+
+            if (scoreCheck)
+            {
+               NewScore(score, playerName);
+            } else
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+            
+        }
+        public static void NewScore(int score, string playerName)
+        {
+            int highscore = 20;
+            string highscorePlayer = "Nikki";
+
+            if(score > highscore)
+            {
+                highscore = score;
+                highscorePlayer = playerName;
+                Console.WriteLine("Congratulations! You beat the highscore!");
+                Console.WriteLine("New score, {0} by {1}", highscore, highscorePlayer);
+            } else if (score == highscore)
+            {
+                Console.WriteLine("You tied for highscore! Try getting points higher than ({0}) to get the highscore place!", highscore);
+            } else
+            {
+                Console.WriteLine("You did not get enough to beat the highscore ({0}). Try again!", highscore);
+            }
         }
     }
 }
